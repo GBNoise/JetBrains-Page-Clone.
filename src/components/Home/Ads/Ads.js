cd6e23d4debbb1e7a7a5b1b29c904f819a5f5903
GBNoise <gbolanos2002@gmail.com>
2022-04-01 05:04:30 -0600

    created Ads component

--- src/components/Home/Ads/Ads.js
+++ src/components/Home/Ads/Ads.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Ads.css";
+import letter from "./images/letter.svg";
+import mascot from "./images/mascot.svg";
+import triangle from "./images/triangle.svg";
+
+const images = [letter, mascot, triangle];
 
 const smallAds = [
   {
@@ -25,12 +30,13 @@ export const Ads = () => {
     <section className="ads_container">
       <Ad
         title={"JetBrains Annual Highlights 2022"}
-        subtitle={"The year's top stories in one convenient place"}
+        subtitle={"The year's top stories in one convenient place."}
         description={
           "Discover the latest information on our products, team and community"
         }
         btn={"Check it out"}
         size={"bigAd"}
+        images={images}
       />
       <div className="small_container">
         {smallAds.map((ad) => {
@@ -49,13 +55,21 @@ export const Ads = () => {
   );
 };
 
-const Ad = ({ title, subtitle, description, btn, size }) => {
+const Ad = ({ title, subtitle, description, btn, size, images }) => {
   return (
     <div className={size}>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
       <p>{description}</p>
       <button>{btn}</button>
+      {images && (
+        <div className="images_container">
+          {images.map((img, i) => {
+            return <img src={img}></img>;
+          })}
+          <div className="rectangle"></div>
+        </div>
+      )}
     </div>
   );
 };
