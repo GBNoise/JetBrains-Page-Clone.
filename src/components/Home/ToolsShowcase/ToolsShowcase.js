94ad8824af1a6a17f16cb06d94d3940ee31b2f85
GBNoise <gbolanos2002@gmail.com>
2022-04-01 05:06:08 -0600

    created ToolsShowcase component

--- src/components/Home/ToolsShowcase/ToolsShowcase.js
+++ src/components/Home/ToolsShowcase/ToolsShowcase.js
@@ -5,11 +5,16 @@ export const ToolsShowcase = ({ obj }) => {
   const { description1, description2, technologies, linkBtn } = obj;
   return (
     <section className="toolsShowcase_container">
+      <span className="sides"></span>
       {description1}
       {description2}
       <ul className="toolsShowcase_ul">
-        {technologies.map((el) => {
-          return <li>{el}</li>;
+        {technologies.map((el, i) => {
+          return (
+            <li key={i}>
+              <a href="#">{el}</a>
+            </li>
+          );
         })}
         <li className="toolsShowcase_linkBtn">{linkBtn}</li>
       </ul>
