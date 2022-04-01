8aa3090a6ee4edae9cb5665c7912e924e3cc790e
GBNoise <gbolanos2002@gmail.com>
2022-04-01 05:05:06 -0600

    created FullContainer component

--- src/components/Home/FullContainer/FullContainer.js
+++ src/components/Home/FullContainer/FullContainer.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./FullContainer.css";
 const colors = {
   darkWhite: "rgb(244, 244, 244)",
   darkGray: "rgb(39, 40, 44)",
   white: "rgb(255,255,255)",
 };
-export const FullContainer = ({ children, color }) => {
+export const FullContainer = ({ children, color, className }) => {
+  const spanRef = useRef(null);
+  useEffect(() => {
+    spanRef.current.style.setProperty("--color", colors[color]);
+    spanRef.current.style.setProperty("--isvisible", "visible");
+  }, [spanRef]);
+
   return (
-    <section className="full_container" style={{ background: colors[color] }}>
+    <section
+      className={`full_container ${className}`}
+      style={{
+        background: colors[color],
+      }}
+    >
+      <span
+        className="full_container_span"
+        style={{ visibility: "hidden" }}
+        ref={spanRef}
+      ></span>
       {children}
     </section>
   );
