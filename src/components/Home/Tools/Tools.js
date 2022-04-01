b2f0c2fae4b3edbfe90560ad552957aecec24576
GBNoise <gbolanos2002@gmail.com>
2022-04-01 05:05:57 -0600

    created Tools component

--- src/components/Home/Tools/Tools.js
+++ src/components/Home/Tools/Tools.js
@@ -3,11 +3,12 @@ import "./Tools.css";
 export const Tools = ({ arr }) => {
   return (
     <div className="tools_container">
-      {arr.map((el) => {
-        const { name, description, option, date } = el;
+      {arr.map((el, i) => {
+        const { name, description, option, date, image } = el;
         return (
-          <div className="single_tool">
+          <div className="single_tool" key={i}>
             <span>
+              {image && <img src={image} alt="img" />}
               {date && <h4>{date}</h4>}
               <h3>{name}</h3>
               <p>{description}</p>
